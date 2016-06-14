ecb261a28053a19ccc76f2895d9a71113ac5db54
Kevin Mokili <kevin.mokili@gmail.com>
2016-06-14 10:17:36 +0200

    ajout filtres

--- src/pizza/index.js
+++ src/pizza/index.js
@@ -5,4 +5,22 @@ export default
 
   angular.module('dtang.pizza', [])
     .controller('PizzaListController', PizzaListController)
+
+
+
+    .filter('toto', function () {
+      // mon filtre toto
+      return function (input) {
+        return parseInt(input) + 10
+      }
+    })
+
+    .filter('bang', function () {
+      // mon filtre bang
+      return function (input, size = 1) {
+        return input + ' ' + '!'.repeat(size)
+      }
+    })
+
+
     .name
