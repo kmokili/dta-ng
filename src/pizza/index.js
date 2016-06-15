3793d24b125266ceafb9a9a141edf883eaa50b70
Kevin Mokili <kevin.mokili@gmail.com>
2016-06-15 10:41:55 +0200

    export PizzaService dans pizza.service.js

--- src/pizza/index.js
+++ src/pizza/index.js
@@ -2,6 +2,7 @@ import angular from 'angular'
 import { PizzaListController } from './pizza-list.controller'
 import { pizzaToppingsFilter } from './pizza-toppings.filter'
 import { PizzaController } from './pizza.controller'
+import { PizzaService } from './pizza.service.js'
 import { Pizza } from './pizza'
 
 export default
@@ -19,22 +20,6 @@ export default
 
     .filter('pizzaToppings', pizzaToppingsFilter)
 
-    .factory('PizzaService', function ($timeout) {
-      // privé
-      var pizzas = [
-        new Pizza({ name: 'un', status: 0, toppings: ['eggs', 'mushrooms'] }),
-        new Pizza({ name: 'deux', status: 0, toppings: [] }),
-        new Pizza({ name: 'trois', status: 1, toppings: ['eggs', 'eggs', 'mushrooms'] }),
-        new Pizza({ name: 'quatre', status: 0 }),
-        new Pizza({ name: 'cinq', status: 1 })
-      ]
-
-      return {
-        getPizzas () {
-          return $timeout(2000)
-            .then(() => pizzas)
-        }
-      }
-    })
+    .factory('PizzaService', PizzaService)
 
     .name
