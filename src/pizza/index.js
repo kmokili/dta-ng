3bc8ddd62421e7bd3d1ce58fbde363137c685388
Kevin Mokili <kevin.mokili@gmail.com>
2016-06-15 10:22:21 +0200

    PizzaService avec timeout de 2 secondes

--- src/pizza/index.js
+++ src/pizza/index.js
@@ -19,16 +19,20 @@ export default
 
     .filter('pizzaToppings', pizzaToppingsFilter)
 
-    .factory('PizzaService', function () {
+    .factory('PizzaService', function ($timeout) {
+      // privé
+      var pizzas = [
+        new Pizza({ name: 'un', status: 0, toppings: ['eggs', 'mushrooms'] }),
+        new Pizza({ name: 'deux', status: 0, toppings: [] }),
+        new Pizza({ name: 'trois', status: 1, toppings: ['eggs', 'eggs', 'mushrooms'] }),
+        new Pizza({ name: 'quatre', status: 0 }),
+        new Pizza({ name: 'cinq', status: 1 })
+      ]
+
       return {
         getPizzas () {
-          return [
-            new Pizza({ name: 'un', status: 0, toppings: ['eggs', 'mushrooms'] }),
-            new Pizza({ name: 'deux', status: 0, toppings: [] }),
-            new Pizza({ name: 'trois', status: 1, toppings: ['eggs', 'eggs', 'mushrooms'] }),
-            new Pizza({ name: 'quatre', status: 0 }),
-            new Pizza({ name: 'cinq', status: 1 })
-          ]
+          return $timeout(2000)
+            .then(() => pizzas)
         }
       }
     })
