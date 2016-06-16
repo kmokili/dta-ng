1e0db639b0e8415be36973e69ef7f2f11f5a99de
Kevin Mokili <kevin.mokili@gmail.com>
2016-06-16 14:08:00 +0200

    retrait lignes blanches + lignes commentées inutiles

--- src/index.js
+++ src/index.js
@@ -2,19 +2,6 @@ import angular from 'angular'
 import ngRoute from 'angular-route'
 import PizzaModule from './pizza'
 
-class DtaUserDirectiveController {
-  constructor ($timeout) {
-    console.log('Instanciation du contrôleur')
-    $timeout(() => {
-      this.onAction({
-        $event: {
-          color: this.color
-        }
-      })
-    }, 1000)
-  }
-}
-
 angular.module('dtang', [
   PizzaModule,
   ngRoute
@@ -43,47 +30,5 @@ angular.module('dtang', [
 
       .otherwise('/')
   })
-
-
-
-// .component('pizzaToppings', {
-//   bindings: {
-//     user: '=',
-//     color: '@userColor',
-//     onAction: '&'
-//   },
-//   transclude: true,
-//   template: `
-//     <h2>
-//       <strong ng-transclude></strong>
-//       {{ $ctrl.user }}
-//     </h2>
-//   `,
-//   controller: DtaUserDirectiveController
-// })
-
-// .directive('twoWords', function () {
-//   return {
-//     require: 'ngModel',
-//     link: function (scope, element, attrs, ngModelCtrl) {
-//       ngModelCtrl.$validators.weird = function (value) {
-//         return (value || '').split(' ').length === 2
-//       }
-//     }
-//   }
-// })
-
-//   .run(function ($rootScope) {
-//     $rootScope.user2 = { name: 'Thomas' }
-//     // $rootScope.$on('$routeChangeStart', (event, next, current) => {
-//     //   console.log('$routeChangeStart', event)
-//     // })
-//     $rootScope.test = function ({ color }) {
-//       console.log('TEST', color)
-//     }
-//   })
-
-
-
 
 angular.bootstrap(document, ['dtang'])
--- src/pizza/index.js
+++ src/pizza/index.js
@@ -11,18 +11,13 @@ export default
   angular.module('dtang.pizza', [])
     .controller('PizzaListController', PizzaListController)
     .controller('PizzaController', PizzaController)
-
     .filter('bang', function () {
       // mon filtre bang
       return function (input, size = 1) {
         return input + ' ' + '!'.repeat(size)
       }
     })
-
     .filter('pizzaToppings', pizzaToppingsFilter)
-
     .service('PizzaService', PizzaService)
-
     .component('pizzaToppings', PizzaComponent)
-
     .name
