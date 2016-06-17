d4455f7daa6b912053c804d17ede903b32ea2b9b
Kevin Mokili <kevin.mokili@gmail.com>
2016-06-17 09:58:33 +0200

    création module DtaDragDropModule

---
+++ src/dta-drag-drop/dta-drag.directive.js
@@ -0,0 +1,15 @@
+export function DtaDragDirective () {
+  return {
+    scope: {
+      dtaDrag: '&'
+    },
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+      element[0].addEventListener('dragstart', evt => {
+        scope.$apply(function () {
+          scope.dtaDrag()
+        })
+      }, false)
+    }
+  }
+}
--- src/pizza/index.js
+++ src/pizza/index.js
@@ -1,57 +1,26 @@
 import angular from 'angular'
+
+import { DtaDragDropModule } from '../dta-drag-drop'
+
 import { PizzaListController } from './pizza-list.controller'
-import { pizzaToppingsFilter } from './pizza-toppings.filter'
 import { PizzaController } from './pizza.controller'
-import { PizzaService } from './pizza.service.js'
+import { pizzaToppingsFilter } from './pizza-toppings.filter'
+import { PizzaService } from './pizza.service'
 import { PizzaToppingsComponent } from './pizzaToppings.component'
 
 export default
 
-  angular.module('dtang.pizza', [])
-    .controller('PizzaListController', PizzaListController)
-    .controller('PizzaController', PizzaController)
-    .filter('bang', function () {
-      // mon filtre bang
-      return function (input, size = 1) {
-        return input + ' ' + '!'.repeat(size)
-      }
-    })
-    .filter('pizzaToppings', pizzaToppingsFilter)
-    .service('PizzaService', PizzaService)
-    .component('pizzaToppings', PizzaToppingsComponent)
-    .directive('dtaDrag', function () {
-      return {
-        scope: {
-          dtaDrag: '&'
-        },
-        restrict: 'A',
-        link: function (scope, element, attrs) {
-          element[0].addEventListener('dragstart', evt => {
-            // console.log('drag start')
-            scope.$apply(function () {
-              scope.dtaDrag()
-            })
-          }, false)
-        }
-      }
-    })
-    .directive('dtaDrop', function () {
-      return {
-        scope: {
-          dtaDrop: '&'
-        },
-        restrict: 'A',
-        link: function (scope, element, attrs) {
-          element[0].addEventListener('dragover', evt => {
-            evt.preventDefault()
-          }, false)
-          element[0].addEventListener('drop', evt => {
-            evt.preventDefault()
-            scope.$apply(function () {
-              scope.dtaDrop()
-            })
-          }, false)
-        }
-      }
-    })
-    .name
+angular.module('dtang.pizza', [
+  DtaDragDropModule
+])
+
+  .controller('PizzaListController', PizzaListController)
+  .controller('PizzaController', PizzaController)
+
+  .filter('pizzaToppings', pizzaToppingsFilter)
+
+  .service('PizzaService', PizzaService)
+
+  .component('pizzaToppings', PizzaToppingsComponent)
+
+  .name
